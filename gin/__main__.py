a83d7577179bf2dc8217eb98b6efa6a1bef36f75
nhomble <nhomble@terpmail.umd.edu>
2022-02-25 11:50:25 -0700

    clean up

--- gin/__main__.py
+++ gin/__main__.py
@@ -1,24 +1,4 @@
-#!/usr/bin/env python3
-
-import json
-from os.path import expanduser, join, isdir
-import os
-import stat
-
 from gin.commands import cli
-
-# def main():
-#     print(gin_path_bin())
-#     print(gin_path_repos())
-#     print(get_repos())
-#
-#     add_repo("dev", "local", "~/tmp")
-#     add_repo("dev2", "local", "~/tmp2")
-#
-#     list_gin("dev")
-#     get_gin("dev", "hello")
-#     get_gin("dev2", "hi")
-
 
 if __name__ == "__main__":
     cli()
